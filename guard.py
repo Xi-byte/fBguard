11aba8243dcd97ec6e1c74acebe420019fb1d5fc
José Fernando <79293767+zff7@users.noreply.github.com>
2021-02-19 01:04:52 -0300

    Update guard.py

--- guard.py
+++ guard.py
@@ -1,102 +1,48 @@
-#-*- coding: utf-8 -*-
-# https://m.facebook.com/jf.hk.167
-
-import requests,sys,optparse
-
-class generate_token(object):
-      def __init__(self,user,pwd):
-          self.user = user
-          self.pwd = pwd
-
-      def __dir__(self):
-          return self.generate()['access_token'],self.generate()['uid']
-          
-      def generate(self):
-          r = requests.get(
-            f'https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email={self.user}&locale=en_US&password={self.pwd}&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6'
-          )
-          if 'error_code' in r.json():
-              print(f'\n\033[91m[x]\033[0m Erro_Message: {r.json["error_code"]}\n')
-              exit()
-          else:
-              return r.json()
-
-class Active(generate_token):
-
-      def __init__(self,user,pwd,active=None):
-          super().__init__(user,pwd)
-          self.active = active
-
-      def data(self,x):
-          return 'variables={"0":{"is_shielded":%s,"session_id":"9b78191c-84fd-4ab6-b0aa-19b39f04a6bc","actor_id":"%s","client_mutation_id":"b0316dd6-3fd6-4beb-aed4-bb29c5dc64b0"}}&method=post&doc_id=1477043292367183&query_name=IsShieldedSetMutation&strip_defaults=true&strip_nulls=true&locale=en_US&client_country_code=US&fb_api_req_friendly_name=IsShieldedSetMutation&fb_api_caller_class=IsShieldedSetMutation' % (x,super().__dir__()[1])
-      @property          
-      def headers(self):
-          return {
-            "Content-Type" : "application/x-www-form-urlencoded",
-            "Authorization" : f"OAuth {super().__dir__()[0]}"
-          }
-      def request(self):
-          r = requests.post(
-            'https://graph.facebook.com/graphql',
-            data = self.data(self.active),
-            headers = self.headers
-          )
-          if 'error' in r.json():
-             print(r.json()['error']['message'])
-          else:
-             if self.active == 'true':
-                print('\n\033[92m[+]\033[0m  Your Profile Guard Has Been Enable\n')
-             else:
-                print('\n\033[92m[-]\033[0m Your Profile Guard Has Been Disable\n')
-
-
-banner = '''\033[92m
- ________________________________________
-|                                        |
-|________________________________________|
-|                                        |
-|         contact : zffk7@hotmail.com    |
-|                                        |
-|         project :  Profile Guard       |
-|________________________________________|
-|                                        |
-|________________________________________|'''
-
-par = optparse.OptionParser(   
-usage = 'usage: python %prog --user arg --password arg --enable/--disable'
-)
-par.add_option(
-    '--user',help='Your Facebook Email Or Phone Number',default=None,metavar='user@mail.ex'
-)
-par.add_option(
-    '--password',help='Your Facebook Password',default=None,metavar='password'
-)
-par.add_option(
-    '--enable',help='Turn On Your Facebook Picture Guard',action='store_true'
-)
-par.add_option(
-    '--disable',help='Turn Off Your Facebook Picture Guard',action='store_true'
-)
-(arg,opt) = par.parse_args()
-
-try:
-     if arg.user != None or arg.password != None:
-        print(banner)
-        if arg.enable:
-           Active(arg.user,arg.password,active="true").request()
-        elif arg.disable:
-           Active(arg.user,arg.password,active="false").request()
-        else:
-           par.print_help()
-     else:     
-        par.print_help()
-
-except Exception as e:
-     print(e)
-except KeyboardInterrupt:
-    exit()
-
-
-
-
-
+<?php
+// geci return (csak a consolehez)
+$biru = "\e[34m";
+$kuning = "\e[33m";
+$cyan = "\e[96m";
+$magenta = "\e[35m";
+$hijau = "\e[92m";
+$merah = "\e[91m";
+echo "$hijau  ×============================================×\n";
+echo "$hijau  [  keszitette:  : zff7                 ]\n";
+echo "$hijau  ×============================================ï¿½\n\n";
+echo "$input $magenta FB ACCOUNT GUARD TESO ERTED>> : ";
+$token= trim(fgets(STDIN));
+$md5 = md5(time());
+$hash = substr($md5, 0, 8)."-".substr($md5, 8, 4)."-".substr($md5, 12, 4)."-".substr($md5, 16, 4)."-".substr($md5, 20, 12);
+function curl($url, $post=null) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    if($post != null) {
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    }
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $exec = curl_exec($ch);
+    curl_close($ch);
+    return $exec;
+}
+$me = json_decode(curl("https://graph.facebook.com/me?fields=id&access_token=".$token));
+if($me && $me->id) {
+    $var = "{\"0\":{\"is_shielded\":true,\"session_id\":\"$hash\",\"actor_id\":\"$me->id\",\"client_mutation_id\":\"$hash\"}}";
+    $hajar = json_decode(curl("https://graph.facebook.com/graphql", array(
+        "variables" => $var,
+        "doc_id" => "1477043292367183",
+        "query_name" => "IsShieldedSetMutation",
+        "strip_defaults" => "true",
+        "strip_nulls" => "true",
+        "locale" => "en_US",
+        "client_country_code" => "US",
+        "fb_api_req_friendly_name" => "IsShieldedSetMutation",
+        "fb_api_caller_class" => "IsShieldedSetMutation",
+        "access_token" => $token
+    )));
+    if($hajar->data->is_shielded_set->is_shielded) echo "$cyan Status ==> $hijau Siker, ellenőrizd fb-profilod";
+    else "fuck it :( GECI NÉZD MEG A FASZBUK TOKENED";
+}
+?>
